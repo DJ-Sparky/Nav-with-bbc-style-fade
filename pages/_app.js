1fcefcd46525504ec5c70f6fdb91f5f4412711c2
popbang <popbang@mail.com>
2021-12-18 15:54:47 +0000

    just for fun - changed console message

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import '../styles/app.css';
 import '../styles/globals.scss';
 
 console.log(
-	'%c Get your own code!',
-	'font-size: 50px; text-transform: uppercase; color: white; text-shadow: 2px 2px red, 4px 4px orange, 6px 6px yellow, 8px 8px green, 10px 10px blue, 12px 12px purple;'
+	'%c Does my code look big in this?',
+	'font-size: 30px; text-transform: uppercase; color: white; text-shadow: 2px 2px red, 4px 4px orange, 6px 6px yellow, 8px 8px green, 10px 10px blue, 12px 12px purple;'
 );
 
 class MyApp extends App {
@@ -19,7 +19,6 @@ class MyApp extends App {
 						name='viewport'
 						content='width=device-width,initial-scale=1.0'
 					/>
-					{/* <link href='/dist/output.css' rel='stylesheet'></link> */}
 				</Head>
 				<Layout>
 					<Component {...pageProps} />
